957c8976d6ca61e04b68c03d6bf5c8afc27f36ae
Joe Masilotti <joe@masilotti.com>
2020-07-11 09:49:46 -0400

    5-star reviews section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
   theme: {
     fill: theme => ({
       'gray-100': theme('colors.gray.100'),
-    })
+      'initiative-orange': theme('colors.initiative-orange'),
+    }),
+    extend: {
+      colors: {
+        "initiative-orange": "#e77e29",
+        "attack-red": "#ed3638",
+        "hull-yellow": "#f0e531",
+        "agility-green": "#6abe46",
+        "shield-blue": "#82d1e1",
+        "energy-pink": "#e71583",
+        "stress-red": "#ec1f21",
+        "destress-blue": "#1e90ff",
+      }
+    }
   },
   variants: {
     backgroundColor: ['responsive', 'hover', 'focus', 'active'],
